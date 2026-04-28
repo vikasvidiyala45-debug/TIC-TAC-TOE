625e57020a8bf0aa49e2d12fe253fb76877af769
vikas <vikasvidiyala45@gmail.com>
2026-04-29 01:49:44 +0530

    Create UC-2.java

---
+++ UC-2.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        int toss = rand.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+    static void displayTossResult() {
+        System.out.println("===== Toss Result =====");
+
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first.");
+        }
+
+        System.out.println("Your Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+}
